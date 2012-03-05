0536130622af41508adb539db1baa543c0b006af
Gergely Aradszki <garpeer@gmail.com>
2012-03-05 18:57:39 +0100

    nitial

---
+++ adfly.php
@@ -0,0 +1,39 @@
+<html>
+  <head>
+    <title></title>
+    <meta content="">
+    <?php /*<script>
+var adfly_id = 'example';
+var adfly_advert = 'int';
+//var domains = ['*.testsite.com'];
+var exclude_domains = ['www.testsite.com','sharefiles4u.com','*.example.com','test.test.com'];
+</script> */ ?>
+  </head>
+  <body>
+      <p>Wildcard domains: * can be used in the exclude_domains or domains ( *.example.com matches www.example.com, example.com, foo.bar.baz.example.com)
+      <ul>
+          <li><a href="http://www.google.com">google</a></li>
+          <li><a href="http://www.google.com#hash">google#hash</a></li>
+          <li><a href="http://www.google.com#www.testsite.com">google#www.testsite.com</a></li>
+          <li><a href="http://adf.ly/www.testsite.com">adf.ly/www.testsite.com</a></li>
+          <li><a href="http://www.google.com/path/to/www.testsite.com/#www.testsite.com">google/path/to/testsite.com/#www.testsite.com</a></li>
+          <li><a href="http://testsite.com">TS</a></li>
+          <li><a href="http://www.testsite.com">www.TS</a></li>
+          <li><a href="http://link.testsite.com">link.TS</a></li>
+          <li><a href="https://www.testsite.com">TSs</a></li>
+          <li><a href="http://www.testsite.com/path/to/adf.ly">TS/path/to/adf.ly</a></li>
+          <li><a href="http://www.google.com?q=www.testsite.com">google->tm</a></li>
+          <li><a href="#www.testsite.com">#TS</a></li>
+          <li><a href="">empty</a></li>   
+          <li><a>null</a></li>
+
+          <li><a href="http://test.example.com">test.example</a></li>
+          <li><a href="http://example.com">example</a></li>
+
+          <li><a href="http://foo.test.com">foo.test</a></li>      
+          <li><a href="http://test.test.com">test.test</a></li>
+      </ul>
+      
+      <script type="text/javascript" src="adfly.js?v=<?php echo rand(1, 9999) ?>" ></script>
+  </body>
+</html>
