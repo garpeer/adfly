2e6f9d7a1364e8d871c3a188324f8858eb7fe0f8
Gergely Aradszki <garpeer@gmail.com>
2012-07-04 17:09:15 +0200

    code cleanup

--- adfly.js
+++ adfly.js
@@ -31,99 +31,106 @@
  *   @todo allow single replace (options.parent is <a>)
  */
 var Adfly = function (id, options) {
-    "use strict";    
+    "use strict";
     var self,
-    is_instance,
-    /**
-     * check whether domain is on the domains list
-     *
-     * @param domain
-     * @param domains
-     * @return boolean
-     */
-    has_domain = function (domain, domains) {
-        var d;
-        if (domains) {
-            for (d in domains) {
-                var checker = domains[d];
-                if (domain === checker.host) {
-                    return true;
-                } else if (checker.is_wildcard) {
-                    var start = domain.length - checker.host.length;
-                    if (domain.substr(start - 1) === ('.' + checker.host)) {
-                        return true;
+        is_instance,
+        /**
+        * check whether domain is on the domains list
+        *
+        * @param domain
+        * @param domains
+        * @return boolean
+        */
+        has_domain = function (domain, domains) {
+            var d, checker, start;
+            if (domains) {
+                for (d in domains) {
+                    if (domains.hasOwnProperty(d)) {
+                        checker = domains[d];
+                        if (domain === checker.host) {
+                            return true;
+                        }
+                        if (checker.is_wildcard) {
+                            start = domain.length - checker.host.length;
+                            if (domain.substr(start - 1) === ('.' + checker.host)) {
+                                return true;
+                            }
+                        }
                     }
                 }
             }
-        }
-        return false;
-    },
+            return false;
+        },
 
-    /**
-     * prepare domain list
-     *
-     * @param domains
-     * @return object
-     */
-    prepare_domains = function (domains) {
-        var out, list, i=0;
-        if (domains) {
-            out = {}
-            if ('string' === typeof(domains)) {
-                list = [domains];
-            } else {
-                list = domains;
-            }
-            var l = list.length;
-            for (i; i < l; i++) {
-                var domain = list[i];
-                if ('*' === domain.substr(0,1)) {
-                    out[i] = {
-                        host: domain.substr(2),
-                        is_wildcard: true
-                    }
+        /**
+        * prepare domain list
+        *
+        * @param domains
+        * @return object
+        */
+        prepare_domains = function (domains) {
+            var out,
+                list,
+                i = 0,
+                l,
+                domain;
+            if (domains) {
+                out = {};
+                if ('string' === typeof (domains)) {
+                    list = [domains];
                 } else {
-                    out[i] = {
-                        host: domain,
-                        is_wildcard: false
+                    list = domains;
+                }
+                l = list.length;
+                for (i; i < l; i += 1) {
+                    domain = list[i];
+                    if ('*' === domain.substr(0, 1)) {
+                        out[i] = {
+                            host: domain.substr(2),
+                            is_wildcard: true
+                        };
+                    } else {
+                        out[i] = {
+                            host: domain,
+                            is_wildcard: false
+                        };
                     }
                 }
             }
-        }
-        return out;
-    },
+            return out;
+        },
 
-    /**
-     * parse options
-     *
-     * @param options object
-     * @return object
-     */
-    parseOptions = function (options) {
-        options = (options && Object === options.constructor) ? Object.create(options) : {};
-        if (options.type != 'int' && options.type != 'banner') {
-            options.type = 'int';
-        }
-        options.parent = (undefined !== options.parent) ? options.parent : document;
-        options.domains = prepare_domains(options.domains);
-        options.exclude = prepare_domains(options.exclude);
-        return options;
-    },
+        /**
+        * parse options
+        *
+        * @param options object
+        * @return object
+        */
+        parseOptions = function (options) {
+            options = (options && Object === options.constructor) ? Object.create(options) : {};
+            if (options.type !== 'int' && options.type !== 'banner') {
+                options.type = 'int';
+            }
+            options.parent = (undefined !== options.parent) ? options.parent : document;
+            options.domains = prepare_domains(options.domains);
+            options.exclude = prepare_domains(options.exclude);
+            return options;
+        },
 
-    /**
-     * initialize script
-     *
-     * @param id adfly user id
-     * @param options
-     */
-    init = function (id , options) {
-        if (undefined === id) {
-            throw 'Adfly id not set';
-        }
-        self.id = id;
-        self.options = options;
-    };
-    
+        /**
+        * initialize script
+        *
+        * @param id adfly user id
+        * @param options
+        */
+        init = function (id, options) {
+            if (undefined === id) {
+                throw 'Adfly id not set';
+            }
+            self.id = id;
+            self.options = options;
+        };
+
     if (undefined === this || window === this) {
         self = window.Adfly;
         is_instance = false;
@@ -131,36 +138,35 @@ var Adfly = function (id, options) {
         self = this;
         is_instance = true;
     }
-    
+
     /**
      * replace links
      *
      * @parent parent element (optional)
      */
     self.replace = function (parent) {
-        var options = parseOptions(self.options);
-        parent = (undefined !== parent) ? parent : options.parent;
-        var url;
-        var location = window.location.href;
-        var l = location.length;
-        var links = parent.getElementsByTagName('a');
-        var num_of_links = links.length;
-        var counter = 0;
-        for (var i=0; i < num_of_links; i++) {
-            var link = links[i];
-            var href = (undefined === link.adflyOriginalHref) ? link.href : link.adflyOriginalHref;
-            var hostname = (undefined === link.adflyOriginalHostname) ? link.hostname : link.adflyOriginalHostname;
-            if ( href
-                && ( location != href.substr(0,l) )
-                && ( hostname != 'adf.ly' )
-                ) {
-                var replace = true;
-                if (typeof(options.domains) === "object") {
+        var options = parseOptions(self.options),
+            element = ((undefined !== parent) ? parent : options.parent),
+            location = window.location.href,
+            links = element.getElementsByTagName('a'),
+            link,
+            counter = 0,
+            i = 0,
+            href,
+            hostname,
+            replace;
+        for (i; i < links.length; i += 1) {
+            link = links[i];
+            href = (undefined === link.adflyOriginalHref) ? link.href : link.adflyOriginalHref;
+            hostname = (undefined === link.adflyOriginalHostname) ? link.hostname : link.adflyOriginalHostname;
+            if (href && (location !== href.substr(0, location.length)) && (hostname !== 'adf.ly')) {
+                replace = true;
+                if (typeof (options.domains) === "object") {
                     if (!has_domain(hostname, options.domains)) {
                         replace = false;
                     }
-                } else if (typeof(options.exclude) === "object") {
-                    if ( has_domain(hostname, options.exclude) ) {
+                } else if (typeof (options.exclude) === "object") {
+                    if (has_domain(hostname, options.exclude)) {
                         replace = false;
                     }
                 }
@@ -168,23 +174,22 @@ var Adfly = function (id, options) {
                 if (replace) {
                     link.adflyOriginalHref = href;
                     link.adflyOriginalHostname = hostname;
-                    if (options.type == 'int') {
-                        url = "http://adf.ly/" + id + "/"+href;
+                    if (options.type === 'int') {
+                        link.href = "http://adf.ly/" + id + "/" + href;
                     } else {
-                        url = "http://adf.ly/" + id + "/banner/"+href;
+                        link.href = "http://adf.ly/" + id + "/banner/" + href;
                     }
-                    link.href = url;
-                    counter++;
+                    counter += 1;
                 }
             }
         }
         return counter;
-    }
-    
+    };
+
     if (is_instance) {
         init(id, options);
     } else {
         init(id, options);
         self.replace();
     }
-}
+};
